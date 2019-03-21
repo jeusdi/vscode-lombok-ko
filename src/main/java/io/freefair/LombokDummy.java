c301230c0bde0a916ab6fad322d543adf89dd796
Jordi Cabré <jordi.cabre@external.t-systems.com>
2019-03-21 11:49:38 +0100

    LOG field is not detected

---
+++ src/main/java/io/freefair/LombokDummy.java
@@ -0,0 +1,14 @@
+package io.freefair;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LombokDummy {
+
+    public void dummy() {
+        this.LOG.info("info");
+    }
+
+}
+
